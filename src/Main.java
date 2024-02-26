c62d7e73229403db3f7c48ba38c03d3637fd2c05
num13 <num13-ne@mail.ru>
2024-02-26 16:10:43 +0300

    changed code

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,12 @@
-import java.io.*;
+
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
-        //Объявление переменных payed - заплачено; сhek - сумма расчета бонуса; balance - текущий баланс; bonus - начисляемый бонус
-        System.out.println("Введите суммы пополнения:");
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String tmp = reader.readLine();
-        int payed = Integer.parseInt(tmp);
-        int chek = 100;
-        int balance = 5000;
-        int bonus = 0;
+        int payed = 1027; // уплаченная сумма
+        int chek = 100; //сумма расчета бонуса
+        int balance = 5000; //текущий баланс
+        int bonus = 0; //начисляемый бонус
 
         //Расчет бонуса и вывод итоговой суммы пополнения баланса
         if (payed > 1000) {
